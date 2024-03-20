689758da4ba82d7df49734c6679a869863922fc0
Lukas Rapp <98412992+lukas-rapp@users.noreply.github.com>
2024-03-20 23:57:16 +0100

    Xcode Init

---
+++ Tessa - Teaching Assistant/Tessa - Teaching Assistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tessa - Teaching Assistant
+//
+//  Created by Lukas Rapp on 20.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
